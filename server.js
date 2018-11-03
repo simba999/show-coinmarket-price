09299d3f27ec43c80dd492626dbe1945635bb510
newsimba <simba1512@yahoo.com>
2018-11-03 14:42:36 -0400

    update

--- server.js
+++ server.js
@@ -31,10 +31,7 @@ app.use((req, res, next) => {
 
 app.set('port', (5000))
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
-
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.post('/prices/new', (req, res) => {
     pusher.trigger( 'coin-prices-development', 'prices', {
         prices: req.body.prices
